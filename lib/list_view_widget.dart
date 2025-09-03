ea3198afe4d57a20e3809b92eab884e50152084a
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-09-03 19:56:46 +0530

    vertical and horizontal list items add

--- lib/list_view_widget.dart
+++ lib/list_view_widget.dart
@@ -12,20 +12,112 @@ class ListViewWidget extends StatelessWidget {
         backgroundColor: Colors.orange[300],
       ),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Expanded(
-            child: ListView(
-              children: [
-                Container(
-                  height: 200,
-                  width: 400,
-                  color: Colors.red,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 200,
+              child: ListView(
+                padding: EdgeInsets.all(16.0),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Container(
+                    width: 180,
+                    margin: EdgeInsets.all(8.0),
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    width: 180,
+                    margin: EdgeInsets.all(8.0),
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    width: 180,
+                    margin: EdgeInsets.all(8.0),
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    width: 180,
+                    margin: EdgeInsets.all(8.0),
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    width: 180,
+                    margin: EdgeInsets.all(8.0),
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    width: 180,
+                    margin: EdgeInsets.all(8.0),
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    width: 180,
+                    margin: EdgeInsets.all(8.0),
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    width: 180,
+                    margin: EdgeInsets.all(8.0),
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    width: 180,
+                    margin: EdgeInsets.all(8.0),
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+                child: ListView(
+                  scrollDirection: Axis.vertical,
+                  padding: EdgeInsets.all(16.0),
+                  children: [
+                    Container(
+                      height: 200,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 200,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 200,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 200,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 200,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 200,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 200,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 200,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 200,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 200,
+                      color: Colors.red,
+                    ),
+                  ],
                 )
-              ],
-            )
-          ),
-        ),
+            ),
+          ],
+        )
       ),
     );
   }
