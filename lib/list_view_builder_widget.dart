0ef2495059179be7ce6e83c4ec6c74576c8ef83e
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-09-04 11:08:36 +0530

    page add

---
+++ lib/list_view_builder_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ListViewBuilderWidget extends StatelessWidget {
+  const ListViewBuilderWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ListView.builder Widgets'),
+        centerTitle: true,
+        backgroundColor: Colors.orange[300],
+        actions: [Icon(Icons.navigate_next, size: 40,)],
+        actionsPadding: EdgeInsets.only(right: 10.0),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(16.0),
+        color: Colors.red[100],
+      )
+      //ListView.builder(itemBuilder: itemBuilder),
+    );
+  }
+}
