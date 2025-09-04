01e84b7fdb3e23768d484016ef945720d8dc60da
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-09-04 15:25:10 +0530

    class model add to get the data of user

---
+++ lib/user_model.dart
@@ -0,0 +1,16 @@
+class User {
+  final String name;
+  final String profession;
+  final String images;
+
+  User(this.name, this.profession, this.images);
+
+  List users = [
+    User('Grace Anderson', 'Software Engineer', 'person1.jpg'),
+    User('Sophia Brown', "Flutter Developer", 'person2.jpg'),
+    User('Charlotte Harris', "Web Developer", 'person3.jpg'),
+    User('Autumn Lane', "Rect Developer", 'person4.jpg'),
+    User('Sophie Tyler', "Artist", 'person5.jpg'),
+    User('Sierra Stone', "Junior Engineer", 'person6.jpg'),
+  ];
+}
