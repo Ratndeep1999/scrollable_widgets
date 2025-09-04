2cbc9793dc7270a4d676a634b309c9e4d73d5bab
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-09-04 16:05:10 +0530

    user dynamic name and profession add

--- lib/list_view_builder_widget.dart
+++ lib/list_view_builder_widget.dart
@@ -14,17 +14,28 @@ class ListViewBuilderWidget extends StatelessWidget {
         actions: [Icon(Icons.navigate_next, size: 40)],
         actionsPadding: EdgeInsets.only(right: 10.0),
       ),
-      body: ListView.builder(
-        itemCount: users.length,
-        itemBuilder: (BuildContext context, index) {
-          return ListTile(
-            leading: CircleAvatar(
-              backgroundImage: AssetImage(
-                users[index].images, // this fetch the image
-              ),
+      body: Column(
+        children: [
+          Image.asset(users[0].image, width: 200, height: 200),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.all(16.0),
+              itemCount: users.length,
+              itemBuilder: (BuildContext context, index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    //backgroundColor: Colors.transparent,
+                    backgroundImage: AssetImage(
+                      users[index].image, // this fetch the image
+                    ),
+                  ),
+                  title: Text(users[index].name),
+                  subtitle: Text(users[index].profession),
+                );
+              },
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
