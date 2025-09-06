5484c06540f234a8b9af96eb021a054cfe178ae9
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-09-06 09:52:02 +0530

    List tile refactor

--- lib/list_view_builder_widget.dart
+++ lib/list_view_builder_widget.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:scrollablewidgets/user_model.dart';
 
-class ListViewBuilderWidget extends StatelessWidget {
+class ListViewBuilderWidget extends StatefulWidget {
   const ListViewBuilderWidget({super.key});
+
+
+  @override
+  State<ListViewBuilderWidget> createState() => _ListViewBuilderWidgetState();
+}
+
+class _ListViewBuilderWidgetState extends State<ListViewBuilderWidget> {
+  int? selectedIndex; // store index of selected tile
 
   @override
   Widget build(BuildContext context) {
@@ -14,49 +22,80 @@ class ListViewBuilderWidget extends StatelessWidget {
         actions: [Icon(Icons.navigate_next, size: 40)],
         actionsPadding: EdgeInsets.only(right: 10.0),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.all(16.0),
-              itemCount: users.length,
-              itemBuilder: (BuildContext context, index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 4.0),
-                  child: ListTile(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50.0),
-                    ),
-                    minVerticalPadding: 16.0,
-                    tileColor: Colors.grey[300],
-                    /// Picture
-                    leading: CircleAvatar(
-                      radius: 24.0,
-                      backgroundImage: AssetImage(
-                        'assets/images/${users[index].image}',
-                      ),
-                    ),
-                    /// Name
-                    title: Text(users[index].name),
-                    titleTextStyle: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.black,
-                    ),
-                    /// Profession
-                    subtitle: Text(users[index].profession),
-                    subtitleTextStyle: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.black54,
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
+
+      body: ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemCount: users.length,
+        itemBuilder: (BuildContext context, index) {
+          // tile is selected if it's index matches
+          bool isSelected = selectedIndex == index;
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4.0),
+            /// ListView method
+            child: buildListTile(index, isSelected),
+          );
+        },
       ),
     );
+  }
+
+  ListTile buildListTile(int index, bool isSelected) {
+    return ListTile(
+            onTap: () {
+              debugPrint('On Clicked $index');
+              setState(() {
+                // index store to selectedIndex
+                selectedIndex = index;
+              });
+            },
+            onFocusChange: (hasFocus) {
+              debugPrint('Tile $index focused');
+            },
+            selected: isSelected,
+            trailing: isSelected
+                ? Icon(Icons.circle)
+                : Icon(Icons.circle_outlined),
+            // text and icon color when selected
+            selectedColor: Colors.white,
+            selectedTileColor: Colors.black,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            tileColor: Colors.grey[300],
+            minVerticalPadding: 16.0,
+            titleAlignment: ListTileTitleAlignment.center,
+
+            /// Picture
+            leading: CircleAvatar(
+              radius: 26.0,
+              backgroundImage: AssetImage(
+                'assets/images/${users[index].image}',
+              ),
+            ),
+            /// Name
+            title: Text(users[index].name),
+            titleTextStyle: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.w400,
+              color: Colors.black,
+            ),
+            /// Profession
+            subtitle: Text(users[index].profession),
+            subtitleTextStyle: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+              color: Colors.black54,
+            ),
+            horizontalTitleGap: 16.0,
+            // icon and text in between space
+            minLeadingWidth: 16.0,
+            // minTileHeight: 16.0,
+
+            // for web/desktop
+            enableFeedback: true,
+            hoverColor: Colors.grey[500],
+            focusColor: Colors.orange,
+            focusNode: FocusNode(),
+          );
   }
 }
