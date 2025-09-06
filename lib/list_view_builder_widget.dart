4efd8b751220ae19475103a1c520eb488b7fae89
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-09-06 09:52:50 +0530

    List tile refactor to method

--- lib/list_view_builder_widget.dart
+++ lib/list_view_builder_widget.dart
@@ -4,7 +4,6 @@ import 'package:scrollablewidgets/user_model.dart';
 class ListViewBuilderWidget extends StatefulWidget {
   const ListViewBuilderWidget({super.key});
 
-
   @override
   State<ListViewBuilderWidget> createState() => _ListViewBuilderWidgetState();
 }
@@ -31,6 +30,7 @@ class _ListViewBuilderWidgetState extends State<ListViewBuilderWidget> {
           bool isSelected = selectedIndex == index;
           return Padding(
             padding: const EdgeInsets.symmetric(vertical: 4.0),
+
             /// ListView method
             child: buildListTile(index, isSelected),
           );
@@ -39,63 +39,60 @@ class _ListViewBuilderWidgetState extends State<ListViewBuilderWidget> {
     );
   }
 
+  // ListTile method
   ListTile buildListTile(int index, bool isSelected) {
     return ListTile(
-            onTap: () {
-              debugPrint('On Clicked $index');
-              setState(() {
-                // index store to selectedIndex
-                selectedIndex = index;
-              });
-            },
-            onFocusChange: (hasFocus) {
-              debugPrint('Tile $index focused');
-            },
-            selected: isSelected,
-            trailing: isSelected
-                ? Icon(Icons.circle)
-                : Icon(Icons.circle_outlined),
-            // text and icon color when selected
-            selectedColor: Colors.white,
-            selectedTileColor: Colors.black,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            tileColor: Colors.grey[300],
-            minVerticalPadding: 16.0,
-            titleAlignment: ListTileTitleAlignment.center,
+      onTap: () {
+        debugPrint('On Clicked $index');
+        setState(() {
+          // index store to selectedIndex
+          selectedIndex = index;
+        });
+      },
+      onFocusChange: (hasFocus) {
+        debugPrint('Tile $index focused');
+      },
+      selected: isSelected,
+      trailing: isSelected ? Icon(Icons.circle) : Icon(Icons.circle_outlined),
+      // text and icon color when selected
+      selectedColor: Colors.white,
+      selectedTileColor: Colors.black,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      tileColor: Colors.grey[300],
+      minVerticalPadding: 16.0,
+      titleAlignment: ListTileTitleAlignment.center,
 
-            /// Picture
-            leading: CircleAvatar(
-              radius: 26.0,
-              backgroundImage: AssetImage(
-                'assets/images/${users[index].image}',
-              ),
-            ),
-            /// Name
-            title: Text(users[index].name),
-            titleTextStyle: TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.w400,
-              color: Colors.black,
-            ),
-            /// Profession
-            subtitle: Text(users[index].profession),
-            subtitleTextStyle: TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.w400,
-              color: Colors.black54,
-            ),
-            horizontalTitleGap: 16.0,
-            // icon and text in between space
-            minLeadingWidth: 16.0,
-            // minTileHeight: 16.0,
+      /// Picture
+      leading: CircleAvatar(
+        radius: 26.0,
+        backgroundImage: AssetImage('assets/images/${users[index].image}'),
+      ),
 
-            // for web/desktop
-            enableFeedback: true,
-            hoverColor: Colors.grey[500],
-            focusColor: Colors.orange,
-            focusNode: FocusNode(),
-          );
+      /// Name
+      title: Text(users[index].name),
+      titleTextStyle: TextStyle(
+        fontSize: 18.0,
+        fontWeight: FontWeight.w400,
+        color: Colors.black,
+      ),
+
+      /// Profession
+      subtitle: Text(users[index].profession),
+      subtitleTextStyle: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+        color: Colors.black54,
+      ),
+      horizontalTitleGap: 16.0,
+      // icon and text in between space
+      minLeadingWidth: 16.0,
+      // minTileHeight: 16.0,
+
+      // for web/desktop
+      enableFeedback: true,
+      hoverColor: Colors.grey[500],
+      focusColor: Colors.orange,
+      focusNode: FocusNode(),
+    );
   }
 }
