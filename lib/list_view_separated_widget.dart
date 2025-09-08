0002e341bb3027bde4df6a13295d46a36b5d99e8
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-09-08 13:10:12 +0530

    List Tile add, leading, tile color and title-subtitle add

---
+++ lib/list_view_separated_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:scrollablewidgets/user_model.dart';
+
+class ListViewSeparatedWidget extends StatelessWidget {
+  const ListViewSeparatedWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ListView.separated Widgets'),
+        centerTitle: true,
+        backgroundColor: Colors.orange[300],
+        actions: [Icon(Icons.navigate_next, size: 40)],
+        actionsPadding: EdgeInsets.only(right: 10.0),
+      ),
+      body: SafeArea(
+        child: ListView.separated(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          cacheExtent: 0.0,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage('assets/images/person39.jpg'),
+                radius: 30,
+              ),
+              title: Text('Title'),
+              subtitle: Text('Description of Title'),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              tileColor: Colors.yellow[100],
+              contentPadding: EdgeInsets.symmetric(
+                vertical: 4.0,
+                horizontal: 16.0,
+              ),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return Divider();
+          },
+          itemCount: users.length,
+        ),
+      ),
+    );
+  }
+}
