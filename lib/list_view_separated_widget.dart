cc8a9f9ba55161665fd712670945e2918283b23a
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-09-08 15:19:44 +0530

    After refactor of tile item toggle selection add

--- lib/list_view_separated_widget.dart
+++ lib/list_view_separated_widget.dart
@@ -10,8 +10,17 @@ class ListViewSeparatedWidget extends StatefulWidget {
 }
 
 class _ListViewSeparatedWidgetState extends State<ListViewSeparatedWidget> {
-  bool _isTileSelected = false;
-  Set<int> _selectedItems = {};
+  final Set<int> _selectedItems = {};
+
+  /// Method to check the index is
+  void _toggleSelection(int index) {
+    setState(() {
+      _selectedItems.contains(index)
+          ? _selectedItems.remove(index)
+          : _selectedItems.add(index);
+      debugPrint('Selected index : $_selectedItems');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,61 +38,15 @@ class _ListViewSeparatedWidgetState extends State<ListViewSeparatedWidget> {
           cacheExtent: 0.0,
           itemBuilder: (BuildContext context, int index) {
             // Check tile index is added or not if add then tile true
-            _isTileSelected = _selectedItems.contains(index);
+            bool isTileSelected = _selectedItems.contains(index);
             return Padding(
               padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: ListTile(
-                // main content
-                leading: CircleAvatar(
-                  backgroundImage: AssetImage('assets/images/person39.jpg'),
-                  radius: 30,
-                ),
-                title: Text('Title'),
-                subtitle: Text('Description of Title'),
-                trailing: _isTileSelected
-                    ? Icon(
-                        Icons.check_circle,
-                        color: Colors.black,
-                        weight: 50,
-                        size: 25.0,
-                      )
-                    : Icon(Icons.circle, color: Colors.grey[500], size: 25.0),
-                // styling
-                tileColor: Colors.yellow[200],
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16.0),
-                ),
-                contentPadding: EdgeInsets.symmetric(
-                  vertical: 4.0,
-                  horizontal: 16.0,
-                ),
-                textColor: Colors.grey,
-                titleTextStyle: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.w700,
-                  letterSpacing: 2.0,
-                ),
-                subtitleTextStyle: TextStyle(
-                  fontSize: 15.0,
-                  fontWeight: FontWeight.w400,
-                  wordSpacing: 1.5,
-                  height: 1.5,
-                ),
-                // after selected
-                onTap: () {
-                  debugPrint(index.toString());
-                  setState(() {
-                    // If index is already added then remove other wise add it
-                    _selectedItems.contains(index)
-                        ? _selectedItems.remove(index)
-                        : _selectedItems.add(index);
-                    //_selectedItems.add(index);
-                    debugPrint('Stored Index : $_selectedItems');
-                  });
-                },
-                selected: _isTileSelected,
-                selectedTileColor: Colors.yellow,
-                selectedColor: Colors.black,
+
+              /// ListTile Widget
+              child: ListTileWidget(
+                selectedTile: isTileSelected,
+                itemIndex: index,
+                onTap: () => _toggleSelection(index)
               ),
             );
           },
@@ -100,6 +63,70 @@ class _ListViewSeparatedWidgetState extends State<ListViewSeparatedWidget> {
           itemCount: users.length,
         ),
       ),
+    );
+  }
+}
+
+
+/// Refactored Tile Widget
+class ListTileWidget extends StatelessWidget {
+  const ListTileWidget({
+    super.key,
+    required bool selectedTile,
+    required this.itemIndex,
+    required VoidCallback onTap,
+  }) : _isTileSelected = selectedTile,
+       _onTap = onTap;
+
+  final int itemIndex;
+  final bool _isTileSelected;
+  final VoidCallback _onTap;
+
+  // For Content
+  final String _name;
+  final String _profession;
+  final String _image;
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('Acquire memory for Item : ${itemIndex + 1}');
+    return ListTile(
+      // main content
+      leading: CircleAvatar(
+        backgroundImage: AssetImage('assets/images/person39.jpg'),
+        radius: 30,
+      ),
+      title: Text('Title'),
+      subtitle: Text('Description of Title'),
+      trailing: _isTileSelected
+          ? Icon(
+              Icons.check_circle,
+              color: Colors.black,
+              weight: 50,
+              size: 25.0,
+            )
+          : Icon(Icons.circle, color: Colors.grey[500], size: 25.0),
+      // styling
+      tileColor: Colors.yellow[200],
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+      contentPadding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+      textColor: Colors.grey,
+      titleTextStyle: TextStyle(
+        fontSize: 18.0,
+        fontWeight: FontWeight.w700,
+        letterSpacing: 2.0,
+      ),
+      subtitleTextStyle: TextStyle(
+        fontSize: 15.0,
+        fontWeight: FontWeight.w400,
+        wordSpacing: 1.5,
+        height: 1.5,
+      ),
+      // after selected
+      onTap: _onTap,
+      selected: _isTileSelected,
+      selectedTileColor: Colors.yellow,
+      selectedColor: Colors.black,
     );
   }
 }
