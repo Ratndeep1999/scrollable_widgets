91196e14fab868c5ca8332698e7b15cdae512ccd
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-09-09 13:30:09 +0530

    nested_listview add

---
+++ lib/nested_listview.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NestedListview extends StatelessWidget {
+  const NestedListview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(child: Column()),
+    );
+  }
+}
