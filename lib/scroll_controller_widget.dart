2db12db8c4c6e82403ef9a0061e7edcbde44f963
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-09-09 16:44:19 +0530

    horizontal listview add

--- lib/scroll_controller_widget.dart
+++ lib/scroll_controller_widget.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
 class ScrollControllerWidget extends StatelessWidget {
-  const ScrollControllerWidget({super.key});
+   ScrollControllerWidget({super.key});
+  
+  final List<Color> _colors = <Color>[
+    Color(0xFF0061ff),
+    Color(0xFFa9ff68),
+    Color(0xFFffc8c8),
+    Color(0xFFf9c58d),
+    Color(0xFFb5c6e0),
+    Color(0xFFaefb2a),
+    Color(0xFF5cb270),
+    Color(0xFF34073d),
+    Color(0xFFffed00),
+    Color(0xFFeed991),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -18,11 +31,41 @@ class ScrollControllerWidget extends StatelessWidget {
         ],
         actionsPadding: EdgeInsets.only(right: 10.0),
       ),
-      body: SafeArea(child: Column(children: [
-        SizedBox(
-          child: DecoratedBox(decoration: BoxDecoration()),
-        )
-      ],)),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Horizontal List Items
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _colors.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      height: 250,
+                      width: 250,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: _colors[index],
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black45,
+                            spreadRadius: 15.0,
+                            blurRadius: 20.0,
+                            offset: Offset(30.0, 30.0),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+
+              // Vertical List Items
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
