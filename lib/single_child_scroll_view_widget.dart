2f7ade9b2dfef98f6d0540e90745cf3657be409f
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-09-09 13:30:51 +0530

    single_child_scroll_view_widget add

--- lib/single_child_scroll_view_widget.dart
+++ lib/single_child_scroll_view_widget.dart
@@ -5,6 +5,9 @@ class SingleChildScrollViewWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(child: Column()),
+    );
   }
 }
