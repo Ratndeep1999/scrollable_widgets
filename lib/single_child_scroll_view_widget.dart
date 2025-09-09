6a3a5fbffbc61a821317b4312972aca6560af75c
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-09-09 13:55:12 +0530

    boxes add

--- lib/single_child_scroll_view_widget.dart
+++ lib/single_child_scroll_view_widget.dart
@@ -7,7 +7,31 @@ class SingleChildScrollViewWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: SafeArea(child: Column()),
+      body: SafeArea(
+        child: Column(
+          children: [
+            _getBoxHor(Colors.black),
+            _getBoxHor(Colors.purple),
+            _getBoxHor(Colors.blueGrey),
+            _getBoxHor(Colors.yellow),
+            _getBoxHor(Colors.pink),
+            _getBoxHor(Colors.teal),
+            _getBoxHor(Colors.grey),
+            _getBoxHor(Colors.red),
+            _getBoxHor(Colors.blue),
+            _getBoxHor(Colors.orange),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // get horizontal box method
+  Widget _getBoxHor(Color boxColor) {
+    return SizedBox(
+      height: 100.0,
+      width: 100.0,
+      child: DecoratedBox(decoration: BoxDecoration(color: boxColor)),
     );
   }
 }
