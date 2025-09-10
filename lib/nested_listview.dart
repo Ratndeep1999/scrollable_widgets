f42c963305c246ba22d5d601a251e2e8c8c2228d
Ratndeep1999 <ratndeepchandankhede1@gmail.com>
2025-09-10 15:14:03 +0530

    appbar add

--- lib/nested_listview.dart
+++ lib/nested_listview.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
 class NestedListview extends StatelessWidget {
-  const NestedListview({super.key});
+  NestedListview({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: SafeArea(child: Column()),
+      appBar: AppBar(
+        title: Text('Nested ListView'),
+        centerTitle: true,
+        backgroundColor: Colors.orange[300],
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.navigate_next, size: 40),
+          ),
+        ],
+        actionsPadding: EdgeInsets.only(right: 10.0),
+      ),
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Container(height: 200, width: 200, color: Color(0xFF5cb270)),
+          ],
+        ),
+      ),
     );
   }
 }
